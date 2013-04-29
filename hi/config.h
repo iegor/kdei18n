035b234b87e16159231876a864c2a55400ac8e48
Iegor <rmtdev@gmail.com>
2013-04-29 10:59:52 +0000

    re-auto 1.11

---
+++ hi/config.h
@@ -0,0 +1,29 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.in by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "kde-i18n-hi"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* Version number of package */
+#define VERSION "3.5.10"
+
+/* Defined if compiling without arts */
+#define WITHOUT_ARTS 1
